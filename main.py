781ed7880a7c0143eeead8916df7b018f498204f
aivaturi123 <abhivaturi@gmail.com>
2025-12-16 13:05:49 -0500

    local works now

--- main.py
+++ main.py
@@ -13,11 +13,15 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
+    allow_origins=[
+        "http://127.0.0.1:5500",
+        "http://localhost:5500",
+    ],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 class CardRequest(BaseModel):
     url: str
